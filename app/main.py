89aa738e5d074d6852556c9e1c7f2e2a88130947
Callum <callum@sidekickhealth.com>
2023-01-07 15:11:30 +0000

    Use Jinja template

--- app/main.py
+++ app/main.py
@@ -1,20 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
-async def home():
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
     data = {
-        "text": "hi"
+        "page": "Home page"
     }
-    return {"data": data}
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
-@app.get("/page/{page_name}")
-async def page(page_name: str):
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def page(request: Request, page_name: str):
     data = {
         "page": page_name
     }
-    return {"data": data}
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
